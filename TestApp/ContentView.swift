7160c70ced8ebe4705828c47c3eb7f6d38e3f823
Parth <pbarot297@gmail.com>
2023-04-09 11:59:56 -0700

    Code Upload

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Parth Barot on 2/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Parth Barot on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
